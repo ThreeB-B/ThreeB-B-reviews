79bbb3bbe7bfac2799165ae8c1fcb2712087eda6
Joel Carpenter <aldosac@gmail.com>
2020-08-26 13:28:53 -0700

    Completes bigDataGenerator

--- data_generator/bigDataGenerator.js
+++ data_generator/bigDataGenerator.js
@@ -1,1 +1,21 @@
 const fs = require('fs');
+const { roomReviewGenerator } = require('./roomReviewGenerator');
+
+const file = fs.createWriteStream('./bigDataSet.json');
+
+module.exports.bigDataGenerator = (times) => {
+  file.on('error', (err) => {
+    console.log(err);
+  });
+
+  file.write('{');
+
+  for(let i = 0; i < times; i++) {
+    file.write(`\n"${i}":${JSON.stringify(roomReviewGenerator(i))},`);
+  }
+
+  file.write('\n}');
+  file.end();
+};
+
+module.exports.bigDataGenerator(10000000);

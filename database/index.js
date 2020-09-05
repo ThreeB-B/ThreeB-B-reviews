3187bc5833e44aea9a2edbc48975ff9ff151db65
Joel Carpenter <aldosac@gmail.com>
2020-09-05 13:53:43 -0700

    Implements environment variables for db url

--- database/index.js
+++ database/index.js
@@ -4,7 +4,7 @@ const { username, password } = require('./config');
 const db = new Pool({
   user: username,
   password: password,
-  host: 'localhost',
+  host: process.env.DB_URL,
   database: '3bb-reviews',
   port: '5432'
 });

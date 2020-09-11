cccfe2acb523777d2bd85c1fbdc341a2ff3c02f6
Joel Carpenter <aldosac@gmail.com>
2020-09-11 15:55:06 -0700

    Converts to sequelize and preps for db replication

--- database/index.js
+++ database/index.js
@@ -1,14 +1,23 @@
-const { Pool } = require('pg');
-const { username, password } = require('./config');
+const { Sequelize } = require('sequelize');
+const {
+  username,
+  password,
+  rep_user,
+  rep_pass
+} = require('./config');
 
-const db = new Pool({
-  user: username,
-  password: password,
-  host: '172.31.13.236',
-  database: '3bb-reviews',
-  port: '5432'
+const MASTER_HOST = "34.221.63.95";
+const REP1_HOST = "54.201.145.150";
+const REP2_HOST = "34.214.70.79";
+
+module.exports.db = new Sequelize('3bb-reviews', null, null, {
+  dialect: "postgres",
+  port: 5432,
+  replication: {
+    read: [
+      { host: REP1_HOST, username: "rep_user", password: rep_pass},
+      { host: REP2_HOST, username: "rep_user", password: rep_pass}
+    ],
+    write: { host: MASTER_HOST, username: username, password: password }
+  }
 });
-
-module.exports = {
-  db
-}
--- server/controllers/getReviews.js
+++ server/controllers/getReviews.js
@@ -4,10 +4,10 @@ module.exports.getReviews = async (room_id) => {
   let reviews;
 
   try {
-    const results = await db.query(`SELECT u.name, u.profilepicnum as profilePicNum, r.* FROM reviews 
+    const [results, metadata] = await db.query(`SELECT u.name, u.profilepicnum as profilePicNum, r.* FROM reviews 
       as r INNER JOIN users as u ON u.id = r.user_id WHERE r.room_id = ${room_id}`);
 
-    reviews = { room_id: room_id, reviews: results.rows };
+    reviews = { room_id: room_id, reviews: results };
   } catch (err) {
     console.log(err);
   }
